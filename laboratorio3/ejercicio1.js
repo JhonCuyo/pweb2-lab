c0374c729df6a4fdd200d1765335d18565cecbb0
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-04-27 09:17:31 -0500

    agregando new Date para obtener el dia actual

--- laboratorio3/ejercicio1.js
+++ laboratorio3/ejercicio1.js
@@ -2,3 +2,6 @@ function DiaSemana(numeroDia){
     const dias=["Domingo","Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
     return dias=[numeroDia];
 }
+const fechaActual= new Date();
+const numeroDia=fechaActual.getDay();
+console.log(DiaSemana(numeroDia));
