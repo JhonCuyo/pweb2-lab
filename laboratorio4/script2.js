86047049c8b9671b224b02adc579c9d2feba2171
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-05-04 20:39:50 -0500

    agregando la primera parte de la funcion mostrartotal,estableciendo los atributos de la tabla y su contenedor

---
+++ laboratorio4/script2.js
@@ -0,0 +1,16 @@
+function mostrarTotal(){
+    fetch("data.json")
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById("contenedor");
+            container.innerHTML = ""; 
+
+            const table = document.createElement("table");
+            table.border = "1";
+            table.style.borderCollapse = "collapse";
+            table.style.width = "100%";
+
+            
+
+
+}
