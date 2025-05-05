6ec39be596edb04106755066d94a0c6977159684
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-05-05 17:51:57 -0500

    agregando codigo para que el grafico se dibuje con los datos obtenidos y se ponga en el div

--- laboratorio4/script5.js
+++ laboratorio4/script5.js
@@ -7,4 +7,24 @@ function mostrarGraficoCompartivo(){
                 const total = region.confirmed.reduce((sum, item) => sum + parseInt(item.value, 10), 0);
                 datos.push([region.region, total]);
             });
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(()=>{
+                const dataChart = google.visualization.arrayToDataTable(datos);
+                const options = {
+                    title: "Total Confirmados por Región",
+                    hAxis: {
+                        title: 'Región',
+                        slantedText: true,
+                        slantedTextAngle: 45
+                    },
+                    Axis: {
+                        title: 'Casos Confirmados'
+                    },
+                    legend: 'none'
+                };
+                const chart = new google.visualization.PieChart(document.getElementById("grafico"));
+                chart.draw(dataChart, options);
+            });
+        })
+        .catch(error => console.error("error al cargar los datos:", error));
 }
