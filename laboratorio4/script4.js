f9fadf1d05dd42beb7e392f44086c712d9e367dc
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-05-05 16:09:37 -0500

    actualizando el metodo, agregando para que cargue el google charts y los datos por dia

--- laboratorio4/script4.js
+++ laboratorio4/script4.js
@@ -7,6 +7,12 @@ function mostrarGraficoArequipa(){
                 console.error("No se encontró la región Arequipa en los datos.");
                 return;
             }
-
-
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(()=>{
+                const datos =[["Fecha", "Confirmados"]];
+                arequipa.confirmed.forEach(dia => {
+                    datos.push([new Date(dia.date), parseInt(dia.value, 10)]);
+                });
+                const dataChart = google.visualization.arrayToDataTable(datos);
+            
 }
