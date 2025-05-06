f04a669c83285cb0b24fcb57c74690b718086cf4
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-05-05 19:29:45 -0500

    agregar los valores de las regiones menos las excepciones

--- laboratorio4/script6.js
+++ laboratorio4/script6.js
@@ -4,5 +4,11 @@ function mostrarGraficoExcepcion(){
         .then(data => {
         const fechas = data[0].confirmed.map(item => item.date);
         const datos = [["Fecha"]];
+
+        data.forEach(region => {
+            if (region.region !== "Lima" && region.region !== "Callao") {
+                datos[0].push(region.region);
+            }
+        });
         })
 }
