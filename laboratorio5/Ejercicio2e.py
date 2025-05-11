b931bd0154c9703f9e4ef7e36f68e57ba06a5bf6
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-05-11 10:53:58 -0500

    rehuando el codigo del ejercicio2d

---
+++ laboratorio5/Ejercicio2e.py
@@ -0,0 +1,6 @@
+from interpreter import draw
+from chessPictures import *
+cuadroBlanco=Picture(SQUARE)
+cudroNegro=cuadroBlanco.negative()
+join=cuadroBlanco.join(cudroNegro)
+draw(join.horizontalRepeat(4))
