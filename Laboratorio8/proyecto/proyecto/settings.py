8ca6b567b93f5ed071ef70a5a4e9bfd6a1c2682e
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-08 15:57:15 -0500

    configurando el correo para el envio de email en settings

--- Laboratorio8/proyecto/proyecto/settings.py
+++ Laboratorio8/proyecto/proyecto/settings.py
@@ -121,3 +121,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jcuyocc@unsa.edu.pe'
+EMAIL_HOST_PASSWORD = '12345'
