527db147e9ae87bcd747016576218a01b1aa6378
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-08 15:19:45 -0500

    agregando en vistas para ver los libros de un autor

--- Laboratorio8/proyecto/app/views.py
+++ Laboratorio8/proyecto/app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Autor, Estudiante, Curso
 
-# Create your views here.
+def libros_de_autor(request, autor_id):
+    autor = get_object_or_404(Autor, id=autor_id)
+    libros = autor.obtener_libros()
+    return render(request, 'app/libros_autor.html', {'autor': autor, 'libros': libros})
+
