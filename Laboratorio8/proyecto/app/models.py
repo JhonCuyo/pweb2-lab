c6fbbee2b052e6a97b28489f5b90850ac4e69e0e
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-08 14:58:44 -0500

    creando nuestra clases que utiizaremos en la relacion de uno a muchos

--- Laboratorio8/proyecto/app/models.py
+++ Laboratorio8/proyecto/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Uno a muchos
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def obtener_libros(self):
+        return self.libro_set.all()
+
+    def tiene_libros(self):
+        return self.libro_set.exists()
+        
+    def __str__(self):
+        return self.nombre
+    
+class Libro(models.Model):
+    titulo = models.CharField(max_length=100)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
