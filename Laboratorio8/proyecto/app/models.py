3cd98d297aa53799bdb8e0afada1aedefd648f68
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-08 14:59:53 -0500

    clase en models de muchos a muchos

--- Laboratorio8/proyecto/app/models.py
+++ Laboratorio8/proyecto/app/models.py
@@ -19,3 +19,29 @@ class Libro(models.Model):
 
     def __str__(self):
         return self.titulo
+
+# Muchos a muchos
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    cursos = models.ManyToManyField('Curso')
+
+    def obtener_cursos(self):
+        return self.cursos.all()
+
+    def esta_inscrito_en(self, curso):
+        return self.cursos.filter(id=curso.id).exists()
+    
+    def __str__(self):
+        return self.nombre
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def obtener_estudiantes(self):
+        return self.estudiante_set.all()
+
+    def tiene_estudiantes(self):
+        return self.estudiante_set.exists()
+    
+    def __str__(self):
+        return self.nombre
