d0536984f6a7c987dd0b0711ad493ab074129cfc
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-08 15:20:54 -0500

    agregando vista para ver los cursos de un estudiante

--- Laboratorio8/proyecto/app/views.py
+++ Laboratorio8/proyecto/app/views.py
@@ -6,3 +6,7 @@ def libros_de_autor(request, autor_id):
     libros = autor.obtener_libros()
     return render(request, 'app/libros_autor.html', {'autor': autor, 'libros': libros})
 
+def cursos_de_estudiante(request, estudiante_id):
+    estudiante = get_object_or_404(Estudiante, id=estudiante_id)
+    cursos = estudiante.obtener_cursos()
+    return render(request, 'app/cursos_estudiante.html', {'estudiante': estudiante, 'cursos': cursos})
