3f899f7686fe083fafe25ef045ce42137add67ae
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-08 16:28:08 -0500

    cambiando unos campos para que el envio del email se ejecute con normalidad

--- Laboratorio8/proyecto/proyecto/settings.py
+++ Laboratorio8/proyecto/proyecto/settings.py
@@ -122,9 +122,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jcuyocc@unsa.edu.pe'
-EMAIL_HOST_PASSWORD = '12345'
+EMAIL_HOST_PASSWORD = 'nkju ayox mywq mumv'
