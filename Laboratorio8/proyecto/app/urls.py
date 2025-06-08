771370ea3d8d12d8e63f634b72d03a5f1ec69ac5
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-08 15:54:22 -0500

    agregando la url para generar el pdf en urls.py

--- Laboratorio8/proyecto/app/urls.py
+++ Laboratorio8/proyecto/app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('autor/<int:autor_id>/', views.libros_de_autor, name='libros_de_autor'),
     path('estudiante/<int:estudiante_id>/', views.cursos_de_estudiante, name='cursos_de_estudiante'),
     path('curso/<int:curso_id>/', views.estudiantes_de_curso, name='estudiantes_de_curso'),
+    path('reporte/pdf/', views.generar_pdf, name='generar_pdf'),
 ]
