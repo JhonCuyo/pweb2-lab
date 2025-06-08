e17e86f8f13e2fc541cc19d6c327fe0b177f6151
Jhon Deyvis Cuyo Ccapa <jcuyocc@unsa.edu.pe>
2025-06-08 15:52:03 -0500

    creando la vista que genera el pdf

--- Laboratorio8/proyecto/app/views.py
+++ Laboratorio8/proyecto/app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Autor, Estudiante, Curso
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 def libros_de_autor(request, autor_id):
     autor = get_object_or_404(Autor, id=autor_id)
@@ -15,3 +18,14 @@ def estudiantes_de_curso(request, curso_id):
     curso = get_object_or_404(Curso, id=curso_id)
     estudiantes = curso.obtener_estudiantes()
     return render(request, 'app/estudiantes_curso.html', {'curso': curso, 'estudiantes': estudiantes})
+
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html  = template.render(context_dict)
+    response = HttpResponse(content_type='application/pdf')
+    pisa_status = pisa.CreatePDF(html, dest=response)
+    return response
+
+def generar_pdf(request):
+    context = {'nombre': 'Jhon'}
+    return render_to_pdf('app/pdf.html', context)
